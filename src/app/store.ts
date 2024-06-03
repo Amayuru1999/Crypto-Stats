1b27a8bcec944e7a99c3518e381ce88b24d90981
Amayuru1999 <amarasingheau@gmail.com>
2024-06-04 03:19:10 +0530

    All  Completed

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,23 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import { usersApi } from '../apis/users.api';
-import { authApi } from '../apis/auth.api';
-import auth from '../slices/auth.slice'
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import { usersApi } from "../apis/users.api";
+import { authApi } from "../apis/auth.api";
+import auth from "../slices/auth.slice";
+import { coinbaseApi } from "../apis/coinbase.api";
 
 export const store = configureStore({
   reducer: {
-    [usersApi.reducerPath]:usersApi.reducer,
-    [authApi.reducerPath]:authApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [coinbaseApi.reducerPath]: coinbaseApi.reducer,
     counter: counterReducer,
     auth,
   },
-    middleware:(getDefaultMiddleware)=>
-      getDefaultMiddleware().concat(usersApi.middleware).concat(authApi.middleware)
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(usersApi.middleware)
+      .concat(authApi.middleware)
+      .concat(coinbaseApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
