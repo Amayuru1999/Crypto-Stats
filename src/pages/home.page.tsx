5afbc16affb7ddbce354e6ae501e6d882afdeeba
Amayuru1999 <amarasingheau@gmail.com>
2024-06-04 02:19:31 +0530

    Coinbase Authentication  Completed

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,9 +1,35 @@
+import { Button } from "@mui/material";
 import React from "react";
-import { LoginForm } from "../components/auth/login-form/login-form.component";
 
 
-const HomePage: React.FC=()=>{
-    return <></>;
-}
 
-export{HomePage};
+
+const HomePage: React.FC = () => {
+  
+
+    const handleConnectCoinbase = () => {
+        const url = process.env.REACT_APP_COINBASE_AUTH_URL;
+        if (url) {
+          console.log('Redirecting to:', url);
+          window.location.href = url;
+        } else {
+          console.error('Coinbase auth URL is not defined');
+        }
+      };
+      
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      
+        <Button
+          variant="contained"
+          size="large"
+          onClick={handleConnectCoinbase}
+        >
+          Connect Coinbase
+        </Button>
+      
+    </div>
+  );
+};
+
+export { HomePage };
